245d8c51f5fbadcca2419860293f7174d12ea558
tkoch97 <tkoch.dev@gmail.com>
2025-08-07 18:07:06 -0300

    chore: change Product model from class to record

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,10 +1,4 @@
 namespace RegisterProductsAPI.Models
 {
-  public class Product
-  {
-    public int Id { get; set; }
-    public string Name { get; set; } = string.Empty;
-    public decimal Price { get; set; }
-    public int Stock { get; set; }
-  }
+  public record Product(int Id, string Nome, decimal Price, int Stock);
 }
