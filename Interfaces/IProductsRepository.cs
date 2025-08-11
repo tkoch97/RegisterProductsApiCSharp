9f256d7a85802ee08cb0c4ddee81dcbae31b27f5
tkoch97 <tkoch.dev@gmail.com>
2025-08-11 15:13:27 -0300

    create interface for ProductsRepository

---
+++ Interfaces/IProductsRepository.cs
@@ -0,0 +1,13 @@
+using RegisterProductsAPI.Models;
+
+namespace RegisterProductsAPI.Interfaces
+{
+  public interface IProductRepository
+  {
+    Task<List<Product>> GetAllProductsAsync();
+    Task<Product> GetProductByIdAsync(int id);
+    Task<dynamic> AddNewProductAsync(Product newProduct);
+    Task<dynamic> DeleteProductAsync(int id);
+    Task<dynamic> UpdateProductAsync(int id, Product updatedProduct);
+  }
+}
