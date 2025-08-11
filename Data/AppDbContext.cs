433103ffe3b0c1838b1870bf0d3d909a5084ba9e
tkoch97 <tkoch.dev@gmail.com>
2025-08-11 16:37:32 -0300

    chore: configure program file to handle with DbContext, repository and service

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -5,11 +5,12 @@ namespace RegisterProductsAPI.Data
 {
   public class AppDbContext : DbContext
   {
+
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+
+    }
     public DbSet<Product> Products { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-      optionsBuilder.UseSqlite(connectionString: "DataSource=app.db;Cache=Shared");
-    }
   }
 }
--- Program.cs
+++ Program.cs
@@ -1,6 +1,19 @@
+using RegisterProductsAPI.Data;
+using RegisterProductsAPI.Interfaces;
+using RegisterProductsAPI.Repository;
+using RegisterProductsAPI.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
+
+builder.Services.AddScoped<IProductRepository, ProductsRepository>();
+builder.Services.AddScoped<ProductService>();
 
 var app = builder.Build();
 
